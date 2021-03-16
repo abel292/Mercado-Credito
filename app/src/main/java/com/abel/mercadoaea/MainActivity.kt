08514f36b580dda6eca7c5006965352f5e2c6660
abel <abel.aguilar292@gmail.com>
2021-03-16 18:49:09 -0300

    Creacion de proyecto

---
+++ app/src/main/java/com/abel/mercadoaea/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.abel.mercadoaea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
